5aa72788163490912770277fb5b7f85b9ca801cd
unknown <h202300046@hanwha.com>
2023-03-10 13:02:20 +0900

    1. SignUpViewModel Modified

--- wpftest/ViewModels/SignUpViewModel.cs
+++ wpftest/ViewModels/SignUpViewModel.cs
@@ -1,13 +1,28 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace wpftest.ViewModels
 {
-    public class SignUpViewModel
+    public class SignUpViewModel : ObservableRecipient
     {
+        #region Private Methods
+        private void OnRegister()
+        {
 
+        }
+        #endregion Private Methods
+        #region Public Methods
+        public ICommand RegsiterUserCommand { get; set; }
+        #endregion Public Methods
+        public SignUpViewModel()
+        {
+            this.RegsiterUserCommand = new RelayCommand(OnRegister);
+        }
     }
 }
