22c39166530ba27e7e9cef9a2288b7e90c94f87e
unknown <h202300046@hanwha.com>
2023-03-10 13:03:01 +0900

    1. UserInfo.cs Revised

--- wpftest/Models/UserInfo.cs
+++ wpftest/Models/UserInfo.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.SQLite;
+
 
 namespace wpftest.Models
 {
-    class UserInfo
+    public class UserInfo
     {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string PhoneNum { get; set; }
+
     }
 }
